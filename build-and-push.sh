60463ae9a193bd555ac65a4b0a01c99904dbed89
Erin Swenson-Healey <l@s3r.me>
2017-12-16 11:11:19 -0800

    get image tag from stack output instead of hard coding it

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,6 +4,13 @@ set -e
 ENV_NAME_ARG=$1
 
 $(aws ecr get-login --no-include-email --region us-east-1)
+
+IMAGE_TAG=$(
+    aws ecr \
+        describe-repositories \
+        --repository-names $ENV_NAME_ARG \
+        --query 'repositories[0].repositoryUri' | sed -e 's;\";;g'):latest
+
 docker build -t "$ENV_NAME_ARG" .
-docker tag "$ENV_NAME_ARG:latest" "166875342547.dkr.ecr.us-east-1.amazonaws.com/$ENV_NAME_ARG:latest"
-docker push "166875342547.dkr.ecr.us-east-1.amazonaws.com/$ENV_NAME_ARG:latest"
+docker tag "$ENV_NAME_ARG:latest" $IMAGE_TAG
+docker push $IMAGE_TAG
--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,22 @@ set -e
 
 ENV_NAME_ARG=$1
 
-IMAGE_TAG="166875342547.dkr.ecr.us-east-1.amazonaws.com/prehistorical:latest"
+IMAGE_TAG=$(
+    aws ecr \
+        describe-repositories \
+        --repository-names $ENV_NAME_ARG \
+        --query 'repositories[0].repositoryUri' | sed -e 's;\";;g'):latest
 
 SERVICE_ARN=$(
     aws cloudformation \
-        describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceArn`].OutputValue' \
+        describe-stacks \
+        --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceArn`].OutputValue' \
         --stack-name $ENV_NAME_ARG | jq '.[0]' | sed -e "s;\";;g")
 
 STACK_NAME=$(
     aws cloudformation \
-        describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceStackName`].OutputValue' \
+        describe-stacks \
+        --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceStackName`].OutputValue' \
         --stack-name $ENV_NAME_ARG | jq '.[0]' | sed -e "s;\";;g")
 
 SERVICE_REGION=$(echo $SERVICE_ARN | sed -e "s;.*ecs:;;g" -e "s;:.*;;g")
