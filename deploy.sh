2356c2c2ad0b92dad062ea7102faf7d257e2a695
Erin Swenson-Healey <l@s3r.me>
2017-12-16 10:58:31 -0800

    Updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,31 @@ set -e
 
 ENV_NAME_ARG=$1
 
-IMAGE_TAG="$(aws ecr describe-repositories --query 'repositories[?repositoryName==`$ENV_NAME_ARG`].repositoryUri' | jq '.[0]'):latest"
-REGION=$(aws configure get region)
-STACK_NAME=$(aws cloudformation --region $REGION describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceStackName`].OutputValue' --stack-name $ENV_NAME_ARG | jq '.[0]')
-WORD="foooo"
-TASK_FILE="./cloud-formation/build-$WORD.json"
+IMAGE_TAG="166875342547.dkr.ecr.us-east-1.amazonaws.com/prehistorical:latest"
 
-echo $IMAGE_NAME
-echo $REGION
-echo $STACK_NAME
+SERVICE_ARN=$(
+    aws cloudformation \
+        describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceArn`].OutputValue' \
+        --stack-name $ENV_NAME_ARG | jq '.[0]' | sed -e "s;\";;g")
 
-#sed -e "s;%IMAGE_TAG%;${IMAGE_TAG};g" ./cloud-formation/service-task-definition.json > $TASK_FILE
-sed -i "s/\b%IMAGE_TAG%\b/${IMAGE_TAG}/g" ./cloud-formation/service-task-definition.json > $TASK_FILE
+STACK_NAME=$(
+    aws cloudformation \
+        describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`HelloworldServiceStackName`].OutputValue' \
+        --stack-name $ENV_NAME_ARG | jq '.[0]' | sed -e "s;\";;g")
 
-echo $TASK_FILE
+SERVICE_REGION=$(echo $SERVICE_ARN | sed -e "s;.*ecs:;;g" -e "s;:.*;;g")
 
-#aws ecs register-task-definition --family $ENV_NAME_ARG --cli-input-json "file://$TASK_FILE"
+SERVICE_NAME=$(echo $SERVICE_ARN | sed -e "s;.*/;;g")
+
+TASK_FILE="./cloud-formation/build-$(date +%s).json"
+
+sed -e "s;%IMAGE_TAG%;$IMAGE_TAG;g" \
+    -e "s;%STACK_NAME%;$STACK_NAME;g" \
+    -e "s;%AWSLOGS_REGION%;$SERVICE_REGION;g" \
+    ./cloud-formation/task-definition.json > $TASK_FILE
+
+TASK_REVISION=$(aws ecs register-task-definition --family helloworld-service --cli-input-json "file://$TASK_FILE" | jq '.["taskDefinition"]["revision"]')
+
+aws ecs update-service --cluster $ENV_NAME_ARG --service $SERVICE_NAME --task-definition helloworld-service:$TASK_REVISION
+
+rm $TASK_FILE
