566b1a22ec6effc59db789222e861a3562ef9af5
Erin Swenson-Healey <l@s3r.me>
2017-12-22 07:18:49 -0800

    Add app support for /api path

--- app/server.py
+++ app/server.py
@@ -3,6 +3,7 @@ import socketserver
 import os
 import urllib.parse as urlparse
 import psycopg2
+import time
 
 PORT = os.environ['PORT']
 DATABASE_URL = os.environ['DATABASE_URL']
@@ -11,23 +12,32 @@ parsed = urlparse.urlparse(DATABASE_URL)
 
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        with psycopg2.connect(
-            dbname=parsed.path[1:],
-            user=parsed.username,
-            password=parsed.password,
-            host=parsed.hostname,
-            port=parsed.port) as connection, open('./index.html', 'rb') as f:
+        if self.path.endswith('api'):
+            with psycopg2.connect(
+                dbname=parsed.path[1:],
+                user=parsed.username,
+                password=parsed.password,
+                host=parsed.hostname,
+                port=parsed.port) as connection:
 
-            cursor = connection.cursor()
-            cursor.execute("select * from pg_language;")
-            query_result = cursor.fetchone()
+                cursor = connection.cursor()
+                cursor.execute("select * from pg_language;")
+                query_result = cursor.fetchone()
 
-            self.send_response(200)
-            self.send_header('X-Database-Stuff', str(query_result))
-            self.end_headers()
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
 
-            self.wfile.write(f.read())
-            return
+                fmt = '{{ "timestamp": "{}", "message": "{}" }}'
+                self.wfile.write(fmt.format(round(time.time()), str(query_result)).encode("utf-8"))
+        else:
+            with open('./index.html', 'rb') as f:
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html; charset=utf-8')
+                self.end_headers()
+
+                self.wfile.write(f.read())
+        return
 
 httpd = socketserver.TCPServer(("", int(PORT)), CustomRequestHandler)
 print("Python web server listening on port {}...".format(PORT))
