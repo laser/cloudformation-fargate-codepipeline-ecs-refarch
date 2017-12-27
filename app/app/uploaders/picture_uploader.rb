31020cc06853ee3b7a48a05d3b849d3a697d4d67
Erin Swenson-Healey <l@s3r.me>
2017-12-26 22:30:16 -0800

    Remove fog dependency

--- app/app/uploaders/picture_uploader.rb
+++ app/app/uploaders/picture_uploader.rb
@@ -3,11 +3,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
