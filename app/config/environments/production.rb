b7cf921d2488fcd19eb991b440c002223d2c2099
Erin Swenson-Healey <l@s3r.me>
2017-12-27 12:40:02 -0800

    enable public file server

--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -14,9 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Serve static files
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
